731c3e7185e9b4ccce9bbb018710407bbe42bf5b
Mihaela P <petre.mihaela8@gmail.com>
2019-06-18 23:42:58 +0100

    [GalleryApi] - Add Swagger

--- GalleryApi/GalleryService/Startup.cs
+++ GalleryApi/GalleryService/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 using System;
 
 namespace GalleryService
@@ -35,6 +36,11 @@ namespace GalleryService
 
             services.AddRouting(options => options.LowercaseUrls = true);
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Gallery API", Version = "v1" });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -51,6 +57,13 @@ namespace GalleryService
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gallery API v1");
+            });
+
             app.UseMvc();
         }
     }
